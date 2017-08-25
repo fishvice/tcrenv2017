9e18fb4a0b824c51ec964c3c34c6b32fa8a362fb
Atlantic <atlantic@ices.local>
2017-08-25 13:09:48 +0200

    tcrtools package added as a folder

---
+++ tcrtools/R/encode.R
@@ -0,0 +1,38 @@
+#' @title encode_zchords
+#'
+#' @description
+#'
+#' The function takes x and y values and converts them to a grid whose value
+#' represent the midpoints given dx and dy.
+#'
+#' The main code is shamelessly stolen from siggi with some tiny bit of
+#' stardust added (if only in the name).
+#'
+#' @param x A numerical vector, normally a longitude
+#' @param y A numerical vector, normally a latitude
+#' @param dx Resolution on the x-axis, normally in decimal degrees (default 1 degree)
+#' @param dy Resolution on the y-axis, normally in decimal degrees (default half of the longitude)
+#' @param invalids A logical (default TRUE), convert values outside mercator ranges to NA.
+#'
+#' @return A character vector where the grid "coding" are the midpoints separated
+#' by a colon. This vector can be split up by e.g. the tidyr:separate function.
+#' @export
+#'
+encode_zchords <- function(x, y, dx = 1, dy = 0.5 * dx, invalids = TRUE) {
+
+  x.brks <- seq(floor(min(x)),ceiling(max(x)),dx)
+  x.ints <- findInterval(x, x.brks, all.inside = TRUE)
+  x <- (x.brks[x.ints] + x.brks[x.ints + 1]) / 2
+
+  y.brks <- seq(floor(min(y)),ceiling(max(y)),dy)
+  y.ints <- findInterval(y, y.brks, all.inside = TRUE)
+  y <- (y.brks[y.ints] + y.brks[y.ints + 1]) / 2
+
+  if(invalids) {
+    x <- ifelse(x >= -180 & x <= 180, x, NA)
+    y <- ifelse(y >= -90  & y <= 90 , y, NA)
+  }
+
+  return(paste(round(x,6), round(y,6), sep = ":"))
+
+}
